2a808fb5ccd18433a5b2297f9dc0f772f4a2ce77
Vandmand <vandmandenelias@gmail.com>
2022-09-08 15:50:25 +0200

    Fixed small problems
    Added roads

--- road.js
+++ road.js
@@ -1,24 +1,27 @@
-GOS.createNode('root', 'roadManager', 2, [], class roadManager {
+GOS.createNode('root', 'roadManager', 0, [], class roadManager {
     constructor(){
         this.connection = undefined
     }
     createRoad(city1, city2){
-        GOS.createNode('roadManager', 'road1', 2, [city1,city2], class Mono{
+        GOS.createNode('roadManager', 'road' + city1.name + city2.name, 2, [city1,city2], class Mono{
             constructor(city1,city2) {
                 this.city1 = city1
                 this.city2 = city2
             }
             update(){
                 strokeWeight(22)
-                line(this.city1.x,this.city2.y)
+                line(this.city1.x,this.city1.y,this.city2.x,this.city2.y)
+            }
+            setup(){
+                // city1.addConnection(city2)
             }
         })
     }
     addRoad(node){
-        if(node.name == this.connection.name){
-            return
-        }
         if(this.connection){
+            if(node.name == this.connection.name){
+                return
+            }
             this.createRoad(this.connection,node)
             this.connection = undefined;
         } else {
